aa5f9a6c68a6bd3be3be27db7e9ea9d4f96551a3
VadimSmD <smirnov.v.d@inbox.ru>
2024-09-27 16:54:33 +0300

    Constructor

--- internal/repo/user.go
+++ internal/repo/user.go
@@ -1,6 +1,27 @@
+package main
+import (
+    "fmt"
+    "os"
+    "context"
+    "../entity/user"
+
+    "github.com/jackc/pgx/v5/pgxpool"
+    "github.com/doug-martin/goqu/v9"
+    _ "github.com/doug-martin/goqu/v9/dialect/postgres"
+)
 type UserRepo struct {
     pg *pgxpool.Pool
 }
-func NewUserRepo( pg *npgxpool.Pool) UserRepo { return UserRepo{pgxpool.Pool : npgxpool.Pool} }
+func NewUserRepo( pg *pgxpool.Pool) UserRepo { return &UserRepo{pg} }
 
-func (r Repo) Insert(ctx context.Context, user entity.User) (entity.User, error) {}
+func (r *UserRepo) Insert(ctx context.Context, user entity.User) (entity.User, error) {
+    sql, args, err := r.Builder.Insert("users").Colums("user_id name surname hashed_pass nickname date_reg status").Values(user.id, user.name, user.surname, user.hashed_pass, user.nickname, user.date, user.status).ToSql()
+    if err != nil {
+        return (user, fmt.Errorf("Repo - users - builder: %w", err))
+    }
+    _, err = r.Pool.Exec(ctx, sql, args)
+    if err != nil {
+        return (user, fmt.Errorf("Repo -user - exec: %w", err))
+    }
+    return (user, nil)
+}
