eb10a39acbabecf95c5dac6234edc0c521a6c0b6
VadimSmD <smirnov.v.d@inbox.ru>
2024-10-20 17:07:47 +0300

    Minor Postgres datatype changes

--- internal/migrations/postgres/000001_create_users_table.up.sql
+++ internal/migrations/postgres/000001_create_users_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id serial PRIMARY KEY,
-    name VARCHAR (50) NOT NULL,
-    surname VARCHAR (50) NOT NULL,
-    hashed_pass VARCHAR (300) NOT NULL,
-    nickname VARCHAR (50) UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    surname TEXT NOT NULL,
+    hashed_pass TEXT NOT NULL,
+    nickname TEXT UNIQUE NOT NULL,
     date_reg timestamp with timezone NOT NULL default now(),
-    status VARCHAR (300) NOT NULL
+    status TEXT
 );
